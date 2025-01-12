cd2fb19285f0acfa352933997e9741ea0ba1db93
umbrik <693254+umbrik@users.noreply.github.com>
2025-01-12 13:26:45 +0100

    Add Error handling wrapper and view (#2)

---
+++ Timestone/Views/ErrorView.swift
@@ -0,0 +1,51 @@
+//
+//  ErrorView.swift
+//  Timestone
+//
+//  Created by umbrik on 12.01.2025.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    
+                }
+            }
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
+
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
